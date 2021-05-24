9e5c041fffd6d5fcfa8bbf72291279b263b5ab3e
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-24 13:48:57 -0300

    update

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,11 @@
+import { ImportCategoryController } from './ImportCategoryController';
+
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
+
+const importCategoryUseCase = new ImportCategoryUseCase();
+
+const importCategoryController = new ImportCategoryController(
+    importCategoryUseCase
+);
+
+export { importCategoryController };
