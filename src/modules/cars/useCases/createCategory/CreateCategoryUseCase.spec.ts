6455c5e7f50473067fb10850f70ccec2e5e1e561
marco <marcoavfcc@gmail.com>
2021-07-28 09:15:49 -0300

    working with authenticated the user

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError"
 import { InMemoryCategoriesRepository } from "../../repositories/in-memory/InMemoryCategoriesRepository"
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
@@ -12,5 +13,41 @@ describe("Create Category", () => {
     )
   })
 
-  it("should be able to create a new category", () => {})
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test"
+    }
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description
+    })
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    )
+
+    // console.log(categoryCreated)
+
+    expect(categoryCreated).toHaveProperty("id")
+  })
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description Test"
+      }
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      })
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
 })
