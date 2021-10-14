8f4e03300fdb0b8516b9e7033140f7aed43c4520
marcoavfcc01121979 <marcoavfcc@gmail.com>
2021-10-14 09:18:24 -0300

    working with refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "c67c7f8615939f506bd777d3151caccb",
+  expires_in_token: "15m",
+  secret_refresh_token: "40789c0dc9f5a59f474d14d95e7c73bd",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
+}
